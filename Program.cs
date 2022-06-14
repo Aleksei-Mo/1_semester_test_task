b59fe26c6d2632dd2bb601ccd21b6f685c484a89
Aleksei <doj20@mail.ru>
2022-06-14 22:14:02 +0400

    FilterArray method was added.

--- Program.cs
+++ Program.cs
@@ -8,14 +8,28 @@ Console.WriteLine("Enter your string array below.");
 Console.WriteLine();
 string[] resultArray = new string[lenghtArray];
 FillArray(resultArray);
+Console.WriteLine("Yours array is:");
+PrintArray(resultArray);
+FilterArray(resultArray);
+Console.WriteLine();
+Console.WriteLine("Yours array after filtration with basis = 3:");
+PrintArray(resultArray);
 
 
 
 
 
-
-
-
+void FilterArray(string[] array)
+{
+    int basis = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > basis)
+        {
+            array[i] = string.Empty;
+        }
+    }
+}
 
 void FillArray(string[] array)
 {
@@ -31,4 +45,16 @@ int EnterUserData(string nameUserData)
 {
     Console.Write($"{nameUserData}");
     return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != string.Empty)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    Console.WriteLine();
 }
